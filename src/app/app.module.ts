e03eed17830f59f4b6200990e517396d5a4af57b
parshva45 <parshva45@gmail.com>
2019-02-15 16:12:22 -0500

    Adding games component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GamesComponent } from './games/games.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GamesComponent
   ],
   imports: [
     BrowserModule,
