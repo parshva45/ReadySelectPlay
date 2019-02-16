024344dae5a85a892c6c1ae09bada2abdcef8250
parshva45 <parshva45@gmail.com>
2019-02-16 15:48:40 -0500

    Database setup completed

--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -19,7 +19,34 @@ export class GamesComponent implements OnInit {
       .then(games => this.games = games);
   }
 
+  postGame(i) {
+    this.gameService
+      .getBgg(i)
+      .then(game => {
+        if (game.gameId) {
+          this.gameService
+            .createGame(game)
+            .then(resp => console.log(resp.gameId));
+        }
+      });
+  }
+
+  myLoop(i) {
+    setTimeout(() => {
+      this.postGame(i);
+      i++;
+      if (i <= 2000) {
+        this.myLoop(i);
+      }
+    }, 1000);
+  }
+
   ngOnInit() {
+    // for (let i = 1; i <= 1000; i++) {
+    //   this.postGame(i);
+    // }
+    // const i = 1;
+    // this.myLoop(i);
     this.loadAllGames();
   }
 
--- src/app/services/game.service.client.ts
+++ src/app/services/game.service.client.ts
@@ -11,4 +11,22 @@ export class GameServiceClient {
     return fetch(this.GAME_URL)
       .then(response => response.json());
   }
+
+  getBgg(i) {
+    return fetch('https://bgg-json.azurewebsites.net/thing/' + i)
+      .then(response => response.json());
+  }
+
+  createGame(game) {
+    return fetch(this.GAME_URL, {
+      body: JSON.stringify(game),
+      credentials: 'include', // include, same-origin, *omit
+      method: 'post',
+      headers: {
+        'content-type': 'application/json'
+      },
+      mode: 'cors'
+    }).then(response => response.json());
+  }
+
 }
