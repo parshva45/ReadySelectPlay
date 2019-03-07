1483591eddaa9f6181723f30dd213306350a6f8b
parshva45 <parshva45@gmail.com>
2019-03-07 12:07:21 -0500

    Adding nav-bar and game-details components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GamesComponent } from './games/games.component';
 import {GameServiceClient} from './services/game.service.client';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { GameDetailsComponent } from './game-details/game-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GamesComponent
+    GamesComponent,
+    NavBarComponent,
+    GameDetailsComponent
   ],
   imports: [
     BrowserModule,
