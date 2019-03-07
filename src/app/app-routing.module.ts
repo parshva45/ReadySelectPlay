2a84adac11112a5db68ccb7f5c0dc86e57c0d27a
parshva45 <parshva45@gmail.com>
2019-03-07 12:52:50 -0500

    Adding routers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {GameDetailsComponent} from './game-details/game-details.component';
+import {GamesComponent} from './games/games.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'games', pathMatch: 'full'},
+  { path: 'games', component: GamesComponent},
+  { path: 'details/:gameId', component: GameDetailsComponent},
+  { path: '**', component: GamesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
