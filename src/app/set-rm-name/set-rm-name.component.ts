41a7067ff5fc26e9f559035885c386741c25b14a
Matthew Hantsbarger <m.hantsbarger@comcast.net>
2019-03-09 15:27:52 -0500

    started making the voting screen page and set-room-name page

---
+++ src/app/set-rm-name/set-rm-name.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-set-rm-name',
+  templateUrl: './set-rm-name.component.html',
+  styleUrls: ['./set-rm-name.component.css']
+})
+export class SetRmNameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/voting-screen/voting-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-voting-screen',
+  templateUrl: './voting-screen.component.html',
+  styleUrls: ['./voting-screen.component.css']
+})
+export class VotingScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
