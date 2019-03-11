89754abad5cb37b9c7f6e55c753eeb1938250db6
Matthew Hantsbarger <m.hantsbarger@comcast.net>
2019-03-11 14:30:02 -0400

    added some framework for set-rm-name

--- src/app/set-rm-name/set-rm-name.component.ts
+++ src/app/set-rm-name/set-rm-name.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Location} from '@angular/common';
+import {ActivatedRoute, Router} from '@angular/router';
+import {RoomServiceClient} from '../services/room.service.client';
+import {UserServiceClient} from '../services/user.service.client';
 
 @Component({
   selector: 'app-set-rm-name',
@@ -7,7 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SetRmNameComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private location: Location,
+              private roomService: RoomServiceClient,
+              private userService: UserServiceClient) {
+    this.route.params.subscribe(params => this.getRoomById(params['roomId']));
+  }
+
+  roomId = '';
+  addedPeople = [];
+
+  goBack() {
+    this.location.back();
+  }
+  getRoomById(roomId) {
+    this.roomId = roomId;
+    // this.roomService
+    //   .getRoomById(roomId)
+    //   .then(room => this.getGames(room.games));
+  }
 
   ngOnInit() {
   }
