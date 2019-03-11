7a7e2af9162e0572b4093ba8a841727b14472c21
Matthew Hantsbarger <m.hantsbarger@comcast.net>
2019-03-11 17:20:12 -0400

    added top bar and back button to voting result (goes back home)

--- src/app/voting-result/voting-result.component.ts
+++ src/app/voting-result/voting-result.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-voting-result',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VotingResultComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  goHome() {
+    this.router.navigate(['/home']);
+  }
 
   ngOnInit() {
   }
